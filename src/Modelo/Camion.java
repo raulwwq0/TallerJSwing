eaf706bb2d084596330c151099044d3f69fdb2fe
RaulCasado <raulcasado969@gmail.com>
2022-05-06 17:49:09 +0200

    Camion furgoneta

---
+++ src/Modelo/Camion.java
@@ -0,0 +1,8 @@
+package com.politecnicomalaga.taller;
+
+public class Camion extends Vehiculo{
+    public static final float precio = 40f;
+    public Camion(String matricula,String modelo ,String propietario ,String DniPropietario){
+        super(matricula,modelo,propietario,DniPropietario);
+    }
+}
---
+++ src/Modelo/Furgon.java
@@ -0,0 +1,9 @@
+package com.politecnicomalaga.taller;
+
+public class Furgon extends Vehiculo{
+    public static final float precio = 30f;
+
+    public Furgon(String matricula,String modelo ,String propietario ,String DniPropietario){
+        super(matricula,modelo,propietario,DniPropietario);
+    }
+}
