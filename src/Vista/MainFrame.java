3e971d7c09ccac0c8d620022b4c4072ba858b2e9
Raúl Aguilar <ag21raul@gmail.com>
2022-05-06 18:32:12 +0200

    Creacion formulario de alta (sin terminar)

--- src/Vista/MainFrame.java
+++ src/Vista/MainFrame.java
@@ -17,26 +17,42 @@ public class MainFrame extends JFrame {
     //Enlace al controlador
     // Por ahora nada...
 
-    // Paneles ---------------------------------------------------------------------------------------------------------
+    // Cosas del Panel Base --------------------------------------------------------------------------------------------
     protected JPanel panelBase;
+
+    // Cosas del Panel Menu --------------------------------------------------------------------------------------------
     protected JPanel panelMenu;
-    protected JPanel panelContenido;
-    protected JPanel panelFormularioAlta;
-    protected JPanel panelCobroTrabajos;
-    protected JPanel panelTrabajosCobrados;
-
-    // Labels ----------------------------------------------------------------------------------------------------------
-
-    // TextFields ------------------------------------------------------------------------------------------------------
-
-    // ComboBox --------------------------------------------------------------------------------------------------------
-
-    // TextArea --------------------------------------------------------------------------------------------------------
-
-    // Botones ---------------------------------------------------------------------------------------------------------
     protected JButton botonMenuFormulario;
     protected JButton botonMenuCobro;
     protected JButton botonMenuYaCobrados;
+
+    // Cosas del Panel Contenido ---------------------------------------------------------------------------------------
+    protected JPanel panelContenido;
+
+    // Cosas del Panel Formulario de Alta ------------------------------------------------------------------------------
+    protected JPanel panelFormularioAlta;
+
+    protected JLabel labelTituloFormulario;
+    protected JLabel labelMatricula;
+    protected JTextField tfMatricula;
+    protected JLabel labelDni;
+    protected JTextField tfDni;
+    protected JLabel labelPropietario;
+    protected JTextField tfPropietario;
+    protected JLabel labelModelo;
+    protected JTextField tfModelo;
+    protected JLabel labelDiagnostico;
+    protected JTextArea taDiagnostico;
+    protected JLabel labelSolucion;
+    protected JTextArea taSolucion;
+    protected JLabel labelHorasPrevistas;
+    protected JTextField tfHorasPrevistas;
+
+    // Cosas del Panel para cobrar trabajos ----------------------------------------------------------------------------
+    protected JPanel panelCobroTrabajos;
+
+    // Cosas del Panel para trabajos ya cobrados -----------------------------------------------------------------------
+    protected JPanel panelTrabajosCobrados;
 
 
     // Constructor #####################################################################################################
@@ -44,9 +60,8 @@ public class MainFrame extends JFrame {
         super(title);  //Para que se muestre el título
 
         //Bordes para los paneles
-        Border blackline;
-        blackline = BorderFactory.createLineBorder(Color.black);
-
+        Border blackline = BorderFactory.createLineBorder(Color.black);
+        Border margenes = BorderFactory.createEmptyBorder(GAP, GAP, GAP, GAP);
 
         // Crear el frame.
         this.setExtendedState(java.awt.Frame.MAXIMIZED_BOTH);
@@ -56,9 +71,11 @@ public class MainFrame extends JFrame {
         panelBase = new JPanel(new BorderLayout());
 
         panelMenu = new JPanel(new FlowLayout(FlowLayout.CENTER, GAP, GAP));
-        panelContenido = new JPanel(new FlowLayout(FlowLayout.CENTER, GAP, GAP));
+        panelContenido = new JPanel();
+        panelContenido.setLayout(new BoxLayout(panelContenido, BoxLayout.Y_AXIS));
 
         panelFormularioAlta = new JPanel();
+        panelFormularioAlta.setLayout(new BoxLayout(panelFormularioAlta, BoxLayout.Y_AXIS));
         panelCobroTrabajos = new JPanel();
         panelTrabajosCobrados = new JPanel();
 
@@ -66,6 +83,22 @@ public class MainFrame extends JFrame {
         botonMenuFormulario = new JButton("Alta Trabajo");
         botonMenuCobro = new JButton("Cobro Trabajo de Taller");
         botonMenuYaCobrados = new JButton("Trabajos ya cobrados");
+
+        labelTituloFormulario = new JLabel("ALTA DE NUEVOS VEHICULOS");
+        labelMatricula = new JLabel("Matrícula:");
+        tfMatricula = new JTextField(10);
+        labelDni = new JLabel("DNI Propietario:");
+        tfDni = new JTextField(10);
+        labelPropietario = new JLabel("Propietario:");
+        tfPropietario = new JTextField(10);
+        labelModelo = new JLabel("Modelo:");
+        tfModelo = new JTextField(10);
+        labelDiagnostico = new JLabel("Diagnóstico:");
+        taDiagnostico = new JTextArea(5, 10);
+        labelSolucion = new JLabel("Solución:");
+        taSolucion = new JTextArea(5, 10);
+        labelHorasPrevistas = new JLabel("Horas previstas:");
+        tfHorasPrevistas = new JTextField(10);
 
         // Ahora añadimos los paneles en la frame (ventana).
         this.add(panelBase);
@@ -81,19 +114,38 @@ public class MainFrame extends JFrame {
         panelMenu.add(botonMenuCobro);
         panelMenu.add(botonMenuYaCobrados);
 
+        panelFormularioAlta.add(labelTituloFormulario);
+        panelFormularioAlta.add(labelMatricula);
+        panelFormularioAlta.add(tfMatricula);
+        panelFormularioAlta.add(labelDni);
+        panelFormularioAlta.add(tfDni);
+        panelFormularioAlta.add(labelPropietario);
+        panelFormularioAlta.add(tfPropietario);
+        panelFormularioAlta.add(labelModelo);
+        panelFormularioAlta.add(tfModelo);
+        panelFormularioAlta.add(labelDiagnostico);
+        panelFormularioAlta.add(taDiagnostico);
+        panelFormularioAlta.add(labelSolucion);
+        panelFormularioAlta.add(taSolucion);
+        panelFormularioAlta.add(labelHorasPrevistas);
+        panelFormularioAlta.add(tfHorasPrevistas);
+
 
         // Coloreamos los paneles para diferenciarlos
         panelBase.setBackground(new java.awt.Color(0, 0, 0));
-        panelMenu.setBackground(new java.awt.Color(36, 243, 21));
-        panelContenido.setBackground(new java.awt.Color(3, 252, 252));
-        panelFormularioAlta.setBackground(new java.awt.Color(252, 0, 0));
-        panelCobroTrabajos.setBackground(new java.awt.Color(181, 0, 252));
-        panelTrabajosCobrados.setBackground(new java.awt.Color(255, 169, 37));
+        panelMenu.setBackground(new java.awt.Color(140, 220, 147));
+        panelContenido.setBackground(new java.awt.Color(140, 197, 220));
+        panelFormularioAlta.setBackground(new java.awt.Color(255, 140, 140));
+        panelCobroTrabajos.setBackground(new java.awt.Color(160, 140, 220));
+        panelTrabajosCobrados.setBackground(new java.awt.Color(255, 226, 140));
 
         // Asignamos ciertas propiedades a los componentes y paneles
         panelFormularioAlta.setVisible(true);
         panelCobroTrabajos.setVisible(false);
         panelTrabajosCobrados.setVisible(false);
+
+        panelFormularioAlta.setBorder(margenes);
+
 
         // Añadimos los listeners
         botonMenuFormulario.addActionListener(new ActionListener() {
